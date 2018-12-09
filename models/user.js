2b8d49d97974216bba291a09e693bcaa14ec7969
Raymond <imleeraymond@gmail.com>
2018-12-08 20:41:17 -0800

    Authenticating login

--- models/user.js
+++ models/user.js
@@ -33,6 +33,44 @@ var UserSchema = new mongoose.Schema({
 	}]
 });
 
+UserSchema.statics.findByToken = function(token) {
+	var User = this;
+	var decoded;
+
+	try{
+		// decoded = jwt.verify(token, process.env.JWT_SECRET);
+		decoded = jwt.verify(token, '123123');
+	}catch (e) {
+		return Promise.reject();
+	}
+
+	return User.findOne({
+		'_id': decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
+	})
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+	var User = this;
+
+	return User.findOne({email}).then((user) => {
+		if(!user){
+			return Promise.reject();
+		}
+
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, res) => {
+				if(res){
+					resolve(user);
+				}else{
+					reject();
+				}
+			});
+		});
+	});
+};
+
 UserSchema.methods.generateAuthToken = function(){
 	var user = this;
 	var access = 'auth';
--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,41 @@
-$(document).foundation()
+$(document).foundation();
 
-
-function textFile(){
-	$.ajax({
-		method: "GET",
-		url: "http://localhost:3000/test",
-		dataType: "text",
-		success: handleData,
-		error: function(){
-			alert('error');
-		}
-	})
-}
-
-
-function handleData(data){
-	$('#placeholder').html(data);
-	localStorage.setItem('test', data);
-}
 
 function retrieveLocalStorage(){
 	alert(localStorage.getItem('test'));
 }
+
+function login(){
+	var email = $('#email').val();
+	var password = $('#password').val();
+
+	var credential = {
+		"email": email,
+		"password": password
+	};
+
+	console.log(credential);
+
+	$.ajax({
+		method: "POST",
+		url: "http://localhost:3000/login",
+		data: JSON.stringify(credential),
+		contentType: 'application/json',
+		dataType: "json",
+		success: function(data, status, xhr){
+	        var token = xhr.getResponseHeader('x-auth');
+	        xhr.setRequestHeader('x-auth', token);
+	        storeToken(token);
+	        alert('success');
+		},
+		error: function(){
+			alert('error');
+		}
+	});
+}
+
+function storeToken(token){
+	console.log(token);
+	localStorage.setItem('token', token);	
+}
+
